374b1240deecb3391c32e2eb49a4a809a1f454a8
diemhuong1642000 <diemhuong1642000@gmail.com>
2021-04-05 17:37:55 +0700

    btth

---
+++ BTAP_LOP/Example_JS/example.js
@@ -0,0 +1,9 @@
+function testExternal(){
+    document.getElementById("test_external").innerHTML ="Hello demo external";
+}
+function change_content(){
+    document.getElementById("test_external").innerHTML ="Đây là nội dung mới";
+}
+function changeImage(){
+    document.getElementById("change_image").src ="h2.jpg";
+}
